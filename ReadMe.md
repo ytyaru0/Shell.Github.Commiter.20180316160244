f132792d261ea41134501b5696a0e4ceaf617657
ytyaru0 <ytyaru0@outlook.jp>
2018-03-16 17:00:13 +0900

    初回コミット

---
+++ ReadMe.md
@@ -0,0 +1,65 @@
+﻿# このソフトウェアについて
+
+簡易アップローダ。
+
+以下のアップローダで作成されたDBからパスワードとメアドを取得する。.shにパスワードをハードコーディングせずに済む。
+
+* https://github.com/ytyaru0/GitHub.Uploader.Pi3.Https.201802210700
+* https://github.com/ytyaru0/GitHub.Uploader.Pi3.Https.201803020700
+
+[こちら](https://github.com/ytyaru0/Shell.Github.Commiter.20180309122449)のカレントディレクトリ版。
+
+要`sqlite3`コマンド。
+
+# 使い方
+
+```sh
+$ push some_username
+```
+
+* `push`ファイルを環境変数`PATH`が通っている所に配置する。
+* カレントディレクトリをリポジトリと想定する
+
+## 確認画面の改善
+
+### ReadMe
+
+```sh
+カレントディレクトリに ReadMe.md が存在しません。リポジトリにしたいなら作成して下さい。
+```
+
+間違ってカレントディレクトリが`/`などだった場合も、少しは安心。
+
+### add前
+
+* git status
+* git add -n .
+
+`git status`を追加。`git rm some.py`などで削除だけしたときも変化がわかるようにした。
+
+### push出力
+
+`git push`コマンドがstderrにパスワードを含むURLを出していたのを抑制した。
+
+```sh
+To https://{user}:{pass}@github.com/{user}/{repo}.git
+```
+
+# 開発環境
+
+* [Raspberry Pi](https://ja.wikipedia.org/wiki/Raspberry_Pi) 3 Model B
+    * [Raspbian](https://www.raspberrypi.org/downloads/raspbian/) GNU/Linux 8.0 (jessie)
+        * bash 4.3.30
+
+# ライセンス
+
+このソフトウェアはCC0ライセンスである。
+
+[![CC0](http://i.creativecommons.org/p/zero/1.0/88x31.png "CC0")](http://creativecommons.org/publicdomain/zero/1.0/deed.ja)
+
+利用ライブラリは以下。
+
+Library|License|Copyright
+-------|-------|---------
+[assert.sh](https://github.com/lehmannro/assert.sh)|[LGPL-3.0](https://github.com/lehmannro/assert.sh/blob/master/COPYING.LESSER)|[Copyright (C) 2007 Free Software Foundation, Inc. http://fsf.org/](https://github.com/lehmannro/assert.sh/blob/master/COPYING)
+
---
+++ src/push.sh
@@ -0,0 +1,97 @@
+CheckArguments () {
+    if [ ! -d "$repo_path" ]; then
+        echo "プログラムエラー。ディレクトリではありません。: $repo_path"
+        exit 1
+    fi
+    if [ ! -f "${repo_path}/ReadMe.md" ] && [ ! -f "${repo_path}/ReadMe.txt" ] && [ ! -f "${repo_path}/ReadMe" ] && [ ! -f "${repo_path}/README.md" ] && [ ! -f "${repo_path}/README.txt" ] && [ ! -f "${repo_path}/README" ]; then
+        echo "カレントディレクトリに ReadMe.md が存在しません。リポジトリにしたいなら作成して下さい。: "${repo_path}
+        exit 1
+    fi
+    if [ ! -n "$username" ]; then
+        echo "リポジトリパス、ユーザ名、の順に引数を渡してください。ユーザ名を指定してください。"
+        exit 1
+    fi
+}
+GetPassMail () {
+    local username=$1
+    local db_file=~/root/script/py/GitHub.Uploader.Pi3.Https.201802210700/res/db/GitHub.Accounts.sqlite3
+    local sql="select Password, MailAddress from Accounts where Username='$username';"
+    local this_dir=`dirname $0`
+    local sql_file=${this_dir}/tmp.sql
+    echo $sql > $sql_file
+    local select=`sqlite3 $db_file < $sql_file`
+    rm $sql_file
+    # "|"→"\n"→改行
+    local value=`echo $select | sed -e "s/|/\\\\n/g"`
+    echo -e "$value"
+}
+CheckPassword () {
+    if [ ! -n "$password" ]; then
+        echo "パスワードが見つかりませんでした。DBを確認してください。"
+        exit 1
+    fi
+    if [ ! -n "$mailaddr" ]; then
+        echo "メールアドレスが見つかりませんでした。DBを確認してください。"
+        exit 1
+    fi
+}
+OverwriteConfig () {
+    username=$1
+    password=$2
+    local before="	url = https://github.com/"
+    local after="	url = https://${username}:${password}@github.com/"
+    local config=".git/config"
+    cp "$config" "$config.BAK"
+    sed -e "s%$before%$after%" "$config.BAK" > "$config"
+    rm "$config.BAK"
+}
+CreateRepository () {
+    if [ ! -d ".git" ]; then
+        echo "リポジトリを作成します。"
+        git init
+        #json='{"name":"'${REPO_NAME}'","description":"'${REPO_DESC}'","homepage":"'${REPO_HOME}'"}'it
+        json='{"name":"'${repo_name}'"}'
+        echo $json | curl -u "${username}:${password}" https://api.github.com/user/repos -d @-
+        git remote add origin https://${username}:${password}@github.com/${username}/${repo_name}.git
+    fi
+}
+CheckView () {
+    git status
+    echo "--------------------"
+    git add -n .
+    echo "--------------------"
+    echo commit message入力するとPush。未入力のままEnterキー押下で終了。
+    read answer
+}
+AddCommitPush () {
+    if [ -n "$answer" ]; then
+        git add .
+        git commit -m "$answer"
+        OverwriteConfig "$username" "$password"
+        # stderrにパスワード付URLが見えてしまうので隠す
+        # https://imokuri123.com/blog/2016/01/git-push-output-is-stderr.html
+        # git push origin master
+        git push origin master 2>&1 | grep -v http
+    fi
+}
+
+# $1 Githubユーザ名
+repo_path=`pwd`
+username=$1
+CheckArguments
+[ 0 -ne $? ] && return
+
+# パスワード取得と設定
+pass_mail=(`GetPassMail $username`)
+password=${pass_mail[0]}
+mailaddr=${pass_mail[1]}
+CheckPassword
+git config --local user.name $username
+git config --local user.email "$mailaddr"
+
+# Create, Add, Commit, Push
+repo_name=$(basename $repo_path)
+echo "$username/$repo_name"
+CreateRepository
+CheckView
+AddCommitPush
